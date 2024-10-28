44f1c453db5a3329549446ef5af64a55b7152354
clarafp8 <158521872+clarafp8@users.noreply.github.com>
2024-10-28 14:35:04 +0100

    Add files via upload

---
+++ ejercicio_6/control6.php
@@ -0,0 +1,14 @@
+<?php 
+
+$n= $_GET['num'];
+
+echo "La tabla de multiplicar del $n es: \n";
+
+for ($i = 1; $i<=10; $i++){
+    echo $i . " x " . $n . " = " . $i*$n ."\n\n";
+}
+
+
+    
+?><br>
+<a href="formulario6.html">Atras</a>
